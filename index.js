bd43b099137c1216656ed806607fe2e4fe27c895
Jose <jozylopez27@gmail.com>
2020-01-08 22:00:34 -0600

    shopping list finished

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ $(function() {
 
 
 
-    $('.shopping-item-toggle').on('click', (event) => {
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
         
-        $('.shopping-item').toggleClass('shopping-item__checked')
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked')
     })
 
-    $('.shopping-item-delete').on('click', (event) => {
-        $('li').first().remove()
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+        $(this).closest('li').remove()
     })
 
 
