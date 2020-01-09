3cc08b0e31ab177c2cb7d540177270efda567471
Jose <jozylopez27@gmail.com>
2020-01-08 20:37:21 -0600

    unfinished list

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('hello')
+$(function() {
+    $('.container').on('submit', '#js-shopping-list-form', (event) => {
+        event.preventDefault();
+
+        let itemInput = $('#shopping-list-entry').val();
+
+        $('.shopping-list').append('<li><span class="shopping-item">apples</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+        $('.shopping-item').last().text(itemInput);
+
+
+    })
+
+
+
+    $('.shopping-item-toggle').on('click', (event) => {
+        
+        $('.shopping-item').toggleClass('shopping-item__checked')
+    })
+
+    $('.shopping-item-delete').on('click', (event) => {
+        $('li').first().remove()
+    })
+
+
+})
